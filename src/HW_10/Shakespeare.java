7670052ec30482e42d8ff6f526a693a1946cad0a
LeshchenkoR <Littleleshch@yandex.ru>
2022-01-20 22:16:51 +0300

    first commit

---
+++ src/HW_10/Shakespeare.java
@@ -0,0 +1,25 @@
+package HW_10;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Shakespeare {
+
+    public void FindPhraze(String fileName, String phraze) throws FileNotFoundException {
+        Scanner scan = new Scanner(new FileReader(fileName));
+        int lineCounter = 0;
+        while (scan.hasNextLine()) {
+            String content = scan.nextLine();
+            lineCounter++;
+            if (content.indexOf(phraze) != -1) {
+                System.out.println("Строка " + lineCounter + " звучит так: " + content);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Shakespeare sonet = new Shakespeare();
+        sonet.FindPhraze("D:\\JAVA\\Новая папка\\HomeWork_M10\\src\\HW_10\\Shakespeare.txt", "вклад");
+    }
+}
